d4b606e41163f3912c5116ffa383493bdc6e2f59
DipankarSethi3012 <dipankarsethi3012@gmail.com>
2024-09-10 12:49:16 +0530

    getposts controllers now working and little bit change donr in comment model

--- models/comment.model.js
+++ models/comment.model.js
@@ -1,21 +1,21 @@
-import { text } from "express";
-import mongoose, { mongo } from "mongoose";
-const commentSchema = new mongoose.Schema ( {
-    text : {
-        type : String,
-        required : true,
+import mongoose from 'mongoose';
+
+// Defining  the comment schema
+const commentSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: true,
     },
-    //the comment is published by whom
-    author : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : 'User',
-        required : true,
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
     },
-    //on which post comment has been done
-    post : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : 'Post'
-    }
+    post: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+    },
 });
 
-export default  commentSchema = mongoose.model('Comment', commentSchema);
+
+export const Comment = mongoose.model('Comment', commentSchema);
